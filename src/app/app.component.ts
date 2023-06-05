518cccb5a8e01b34b057a4ab90422dcefa230518
Michal Štecher <m.stecher@cleverbee.com>
2023-06-05 20:54:10 +0200

    feat: MVP - get channel id and return song ids

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { YoutubeService } from './youtube.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'yt-playlist-merger';
+  channelId = '';
+  videos: string[] = [];
+
+  constructor(private youtubeService: YoutubeService) {}
+
+  getPlaylists() {
+    this.youtubeService
+      .getPlaylists(this.channelId)
+      .subscribe((response: any) => {
+        for (const item of response.items) {
+          this.youtubeService
+            .getPlaylistItems(item.id)
+            .subscribe((response: any) => {
+              for (const video of response.items) {
+                this.videos.push(video.snippet.resourceId.videoId);
+              }
+            });
+        }
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // Import this
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { YoutubeService } from './youtube.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [YoutubeService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
